504fa4be483f1f822978f3c177dd3e55aa1f6288
LUISA SOPHIA SIGUERE MENDEZ <lssiguere@correo.url.edu.gt>
2020-10-09 19:02:10 -0600

    API agregada

---
+++ API HUFFMAN/WeatherForecast.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace API_HUFFMAN
+{
+    public class HuffmanApi
+    {
+        public string Nombredelarchivooriginal { get; set }
+        string Nombreyrutadelarchivocomprimido { get; set }
+        int Razondecompresión { get; set }
+        int Factordecompresión { get; set }
+        int Porcentajedereducción{ get; set }
+    }
+}
+
